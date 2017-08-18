9dbc92a22881356ca0da9c8edaf562e95d166a50
MattJohnPearce <mattjohnpearce87@gmail.com>
2017-08-18 23:06:38 +0800

    Update RoundedTwoDecimal.java

--- RoundedTwoDecimal.java
+++ RoundedTwoDecimal.java
@@ -1,17 +1,26 @@
-public static double roundToTwoDec(double areaMM)
-	{
-		double areaToBeRounded;
+/************************************************************************
+ * Purpose: Rounds a real number up to two decimal places by removing   *
+ * the integer part of the real number and moving the decimal place     *
+ * forward two places and removing the integer again leaving only the   *
+ * fraction part. Compare this fraction with 0.444 recurring to         *
+ * determine if the number needs rounding. If so add a 0.01 to the      *
+ * imported number and then cut of whatever is remaing off the fraction.*
+ * Date: 17/08/2017                                                     *
+ * Import: toBeRounded(Real)                                            *
+ * Export: rounded(Real)                                                *
+ * *********************************************************************/
+public static double roundTwoDeci(double toBeRounded)
+{
+	double rounding = 0.0;
+	final double FOUR_NINTHS = 4.0/9.0;
 		
-		areaToBeRounded = (areaMM - (int)areaMM) * 100.0;
-		areaToBeRounded = (areaToBeRounded - (int)areaToBeRounded) * 10.0;
+	rounding = (toBeRounded * 100.0)  - ((int)toBeRounded * 100);
+	
+	if(rounding > FOUR_NINTHS)
+		toBeRounded = toBeRounded + 0.01;
 		
-		if(areaToBeRounded > (40/9))
-		{
-			areaToBeRounded = (10.0 - areaToBeRounded) / 1000.0;
-			areaMM = areaToBeRounded + areaMM;
-		}
+	rounding = (double)((int)(toBeRounded * 100.0)) / 100.0;
 		
-		areaMM = (double)((int)(areaMM * 100.0)) / 100.0;
+	return rounding;
 		
-		return areaMM;
-	}
+}//end roundTwoDeci
